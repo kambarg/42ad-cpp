4e60b3e01d16f356e24a3431be0a536fd90d86cf
Gaukhar Kambarbaeva <gkambarb@lab1r5s12.42abudhabi.ae>
2025-10-29 17:45:18 +0400

    ex01: setup file structure

---
+++ cpp00/ex01/Contact.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkambarb <gkambarb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 17:11:30 by gkambarb          #+#    #+#             */
+/*   Updated: 2025/10/29 17:44:29 by gkambarb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+
+class Contact
+{
+    private:
+        std::string first_name;
+        std::string last_name;
+        std::string nickname;
+        std::string phone_number;
+        std::string darkest_secret;
+    public:
+        Contact();
+        ~Contact();
+        
+        Contact(std::string first_name, std::string last_name, std::string nickname, std::string phone_number, std::string darkest_secret);
+        
+        // member functions
+};
+
+#endif
