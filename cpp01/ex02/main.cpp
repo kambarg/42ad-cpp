81b05f0497bdaf373ece1c949acf4759833c5760
Gaukhar Kambarbaeva <gkambarb@lab2r2s12.42abudhabi.ae>
2026-01-13 11:35:10 +0400

    cpp01/ex02: added

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkambarb <gkambarb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 10:13:29 by gkambarb          #+#    #+#             */
+/*   Updated: 2026/01/13 11:32:59 by gkambarb         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+# include <iostream>
+
+int main(void) 
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+    
+    std::cout << std::endl;
+    std::cout << "The memory address of the string variable: " << &str<< std::endl;
+    std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "The value of the string variable: " << str << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+    std::cout << std::endl;
+}
