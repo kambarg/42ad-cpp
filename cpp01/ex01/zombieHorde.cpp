93545d912b7176813339a11526d94dd5062af47a
kambarg <kambarg@gmail.com>
2026-01-19 14:10:27 +0400

    cpp01/ex01 ex02 fixed #includes

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,27 +6,26 @@
 /*   By: gkambarb <gkambarb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 14:52:56 by gkambarb          #+#    #+#             */
-/*   Updated: 2026/01/15 23:13:41 by gkambarb         ###   ########.fr       */
+/*   Updated: 2026/01/19 13:57:28 by gkambarb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+#include <sstream>
 
-Zombie* zombieHorde( int N, std::string name )
-{
-    if (N <= 0)
-        return NULL;
-    
-    // Allocate N zombies in a single allocation
-    Zombie* horde = new Zombie[N];
-    
-    // Initialize each zombie with the given name
-    for (int i = 0; i < N; i++)
-    {
-        std::stringstream ss;
-        ss << name << "_" << i;
-        horde[i].setName(ss.str());
-    }
-    
-    return horde;
+Zombie *zombieHorde(int N, std::string name) {
+  if (N <= 0)
+    return NULL;
+
+  // Allocate N zombies in a single allocation
+  Zombie *horde = new Zombie[N];
+
+  // Initialize each zombie with the given name
+  for (int i = 0; i < N; i++) {
+    std::stringstream ss;
+    ss << name << "_" << i;
+    horde[i].setName(ss.str());
+  }
+
+  return horde;
 }
--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,26 +6,29 @@
 /*   By: gkambarb <gkambarb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 10:13:29 by gkambarb          #+#    #+#             */
-/*   Updated: 2026/01/15 23:16:57 by gkambarb         ###   ########.fr       */
+/*   Updated: 2026/01/19 13:59:41 by gkambarb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
-int main(void) 
-{
-    std::string str = "HI THIS IS BRAIN";
-    std::string* stringPTR = &str;
-    std::string& stringREF = str;
-    
-    std::cout << std::endl;
-    std::cout << "The memory address of the string variable: " << &str<< std::endl;
-    std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
-    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+int main(void) {
+  std::string str = "HI THIS IS BRAIN";
+  std::string *stringPTR = &str;
+  std::string &stringREF = str;
 
-    std::cout << std::endl;
-    std::cout << "The value of the string variable: " << str << std::endl;
-    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
-    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
-    std::cout << std::endl;
+  std::cout << std::endl;
+  std::cout << "The memory address of the string variable: " << &str
+            << std::endl;
+  std::cout << "The memory address held by stringPTR: " << stringPTR
+            << std::endl;
+  std::cout << "The memory address held by stringREF: " << &stringREF
+            << std::endl;
+
+  std::cout << std::endl;
+  std::cout << "The value of the string variable: " << str << std::endl;
+  std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+  std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+  std::cout << std::endl;
 }
