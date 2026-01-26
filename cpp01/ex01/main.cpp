54dbb83e72c1dcdf0636a836000684fa5cf945c5
Gaukhar Kambarbaeva <gkambarb@lab2r1s14.42abudhabi.ae>
2026-01-26 18:51:42 +0400

    cpp01/ex01: add upper bound limit for N zombies in the horde

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gkambarb <gkambarb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 15:30:00 by gkambarb          #+#    #+#             */
-/*   Updated: 2026/01/13 10:04:37 by gkambarb         ###   ########.fr       */
+/*   Updated: 2026/01/26 18:45:04 by gkambarb         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,20 +14,20 @@
 
 int main()
 {
-    int N = 5;
+    int N = 10;
     
-    std::cout << "Creating a horde of " << N << " zombies..." << std::endl;
     Zombie* horde = zombieHorde(N, "Zombie");
-    
-    std::cout << "\nCalling announce() for each zombie:" << std::endl;
+    if (horde == NULL)
+    {
+        std::cout << "Error: Failed to create zombie horde." << std::endl;
+        return 1;
+    }
+
     for (int i = 0; i < N; i++)
     {
         horde[i].announce();
     }
     
-    std::cout << "\nDeleting the zombie horde..." << std::endl;
     delete[] horde;
-    
-    std::cout << "\nDone!" << std::endl;
     return 0;
 }
--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   zombieHorde.cpp                                    :+:      :+:    :+:   */
@@ -6,21 +6,23 @@
 /*   By: gkambarb <gkambarb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 14:52:56 by gkambarb          #+#    #+#             */
-/*   Updated: 2026/01/19 13:57:28 by gkambarb         ###   ########.fr       */
+/*   Updated: 2026/01/26 18:49:44 by gkambarb         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "Zombie.hpp"
 #include <sstream>
 
-Zombie *zombieHorde(int N, std::string name) {
-  if (N <= 0)
+Zombie *zombieHorde(int N, std::string name) 
+{
+  if (N <= 0 || N >= 10000)
+  {
+    std::cout << "Cannot create horde: N must be greater than 0 and less than 10000." << std::endl; 
     return NULL;
-
-  // Allocate N zombies in a single allocation
+  }
+  
   Zombie *horde = new Zombie[N];
 
-  // Initialize each zombie with the given name
   for (int i = 0; i < N; i++) {
     std::stringstream ss;
     ss << name << "_" << i;
